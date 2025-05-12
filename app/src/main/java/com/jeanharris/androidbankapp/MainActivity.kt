641a9942dba75bd3a68412705ddae41224167455
Jean Michael Harris <jeanmichael.harris@us.giti.com>
2025-05-12 15:50:18 -0400

    Init

--- app/src/main/java/com/jeanharris/androidbankapp/MainActivity.kt
+++ app/src/main/java/com/jeanharris/androidbankapp/MainActivity.kt
@@ -6,23 +6,42 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.jeanharris.androidbankapp.ui.login.LoginScreen
 import com.jeanharris.androidbankapp.ui.theme.AndroidBankAppTheme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            MaterialTheme {
-                LoginScreen()
+            AndroidBankAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidBankAppTheme {
+        Greeting("Android")
     }
 }
